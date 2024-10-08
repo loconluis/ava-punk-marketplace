3f397d91e531b2cb241f61acdc3edc8c6cae04fb
Luis Locon <luis.locon@gmail.com>
2024-10-08 00:44:24 -0600

    FIrst commit

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `${process.env.SEPOLIA_TESTNET_URI}${process.env.INFRA_API_KEY}`,
+      accounts: [`0x${process.env.MTMK_PRIVATE_KEY}`],
+    },
+    holesky: {
+      url: `${process.env.HOLESKY_TESTNET_URI}${process.env.INFRA_API_KEY}`,
+      accounts: [`0x${process.env.DEPLOYER_SIGNER_PRIVATE_KEY}`],
+    },
+  },
+};
+
+export default config;
